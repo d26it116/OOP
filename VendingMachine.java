13d215cbacfdda7683511d4d9834d1d8542cbfd3
Aaryan Bhandari <d26it116@charusat.edu.in>
2026-08-03 20:58:00 +0530

    Vending Machine

---
+++ VendingMachine.java
@@ -0,0 +1,48 @@
+
+import java.util.Scanner;
+
+enum Coin {
+    ONE, TWO, FIVE, TEN
+}
+
+public class VendingMachine {
+
+    public static void main(String[] args) {
+        int total = 0;
+        Scanner sc = new Scanner(System.in);
+
+
+    while(true) {
+
+        System.out.print("Enter coin (ONE, TWO, FIVE, TEN): ");
+        Coin coin;
+        try {
+                coin = Coin.valueOf(sc.next().toUpperCase());
+            } catch (IllegalArgumentException e) {
+                System.out.println("  Not a valid coin, try again.");
+                continue;
+            }
+        
+
+        int value = switch (coin) {
+            case ONE -> 1;
+            case TWO -> 2;
+            case FIVE -> 5;
+            case TEN -> 10;
+        };
+       
+        total += value;
+
+        if (total <15 ) {
+            System.out.println("  Inserted so far: " + total);
+            
+        }
+        else {
+            System.out.println("Paid. Change: " + (total - 15));
+            break;
+        }   
+        
+        // sc.close();
+    }
+}
+}
