c543fcd4df0ef6c6a804301a9dab9d76d8489fa7
Aaryan Bhandari <d26it116@charusat.edu.in>
2026-08-08 23:05:34 +0530

    Card program

---
+++ Practicle-3/CardProject/Driver.java
@@ -0,0 +1,22 @@
+public class Driver {
+    public static void main(String[] args) {
+
+        Card[] cards = new Card[5];
+
+        cards[0] = new Card("Ace", "Spades");
+        cards[1] = new Card("King", "Hearts");
+        cards[2] = new Card("Queen", "Diamonds");
+        cards[3] = new Card("Ace", "Spades");
+
+        for (int i = 0; i < cards.length; i++) {
+
+            for (int j = 0; j < i; j++) {
+
+                if (cards[i].equals(cards[j])) {
+                    System.out.println("a second " + cards[i] +" ->"+ " Duplicate found: " + cards[i]);
+                    return;
+                }
+            }
+        }
+    }
+}
