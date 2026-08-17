fdbb816a72b9e296d4a81eee1f4a9e1df5351867
Aaryan Bhandari <d26it116@charusat.edu.in>
2026-08-17 21:25:37 +0530

    Driver Program

--- Practicle-4/ChatFilterProgram/Driver.java
+++ Practicle-4/ChatFilterProgram/Driver.java
@@ -4,9 +4,9 @@ public class Driver {
     public static void main(String[] args) {
 
         String[] logs = {
-            "10:05 alice Hello there",
-            "10:10 bob How are you",
-            "10:15"
+            "10:05 Aaryan Hello there",
+            "10:10 Ramesh How are you",
+            "10:15 Aaryan I'm good, thanks",
         };
 
         Scanner sc = new Scanner(System.in);
